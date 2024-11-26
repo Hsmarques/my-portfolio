2514ca52ba821ae7d015b33ccb6ff833329f06bb
Hugo Marques <mrhsmar@gmail.com>
2024-11-26 23:07:22 +0000

    Setting it dark

---
+++ src/routes/about.tsx
@@ -0,0 +1,10 @@
+import { A } from "@solidjs/router";
+
+export default function About() {
+  return (
+    <main class="text-center mx-auto text-gray-100 p-4">
+      <h1 class="max-6-xs text-6xl text-gray-400 font-thin uppercase my-16">About Page</h1>
+      <p>I'm Hugo, a Developer from Portugal</p>
+    </main>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { A } from "@solidjs/router";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-400 p-4">
+      <h1 class="max-6-xs text-6xl text-gray-400 font-thin uppercase my-16">Hello!</h1>
+      <p>I'm adding some content here, soon ™️</p>
+    </main>
+  );
+}
