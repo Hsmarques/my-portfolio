a39a4b8ed42f03bffb3f9455dd8fd8139a66b9b2
Hugo Marques <mrhsmar@gmail.com>
2024-11-28 14:12:11 +0000

    added vercel analytics

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,14 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import { inject } from "@vercel/analytics";
 
 export default function App() {
+  inject();
+
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Nav />
           <Suspense>{props.children}</Suspense>
