f1a2a95d442a4c051de9f336e4eb6aec692556a2
Hugo Marques <mrhsmar@gmail.com>
2024-11-28 14:18:14 +0000

    added vercel speed insights

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,16 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
-import "./app.css";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+import Nav from "~/components/Nav";
+
+import "./app.css";
 
 export default function App() {
   inject();
+  injectSpeedInsights();
 
   return (
     <Router
